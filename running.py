9e9424bb66aabda250d37868f516f188897cd9f7
Marcus Caisey <marcus@teckna.com>
2021-12-29 20:20:38 +0000

    feat: support running multiple tests

--- running.py
+++ running.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Callable
+from typing import Callable, Iterable
 
 
 @dataclasses.dataclass(order=False)
@@ -24,8 +24,11 @@ class TestContext:
 TestFunction = Callable[[TestContext], None]
 
 
-def run_test(test: TestFunction) -> TestResult:
-    result = TestResult(test.__name__)
-    t = TestContext(result)
-    test(t)
-    return t.result
+def run_tests(tests: Iterable[TestFunction]) -> list[TestResult]:
+    results = []
+    for test in tests:
+        result = TestResult(test.__name__)
+        t = TestContext(result)
+        test(t)
+        results.append(t.result)
+    return results
--- test_running.py
+++ test_running.py
@@ -1,16 +1,18 @@
 import unittest
 
-from running import TestContext, TestResult, run_test
+from running import TestContext, TestResult, run_tests
 
 
 class TestRunTest(unittest.TestCase):
+    longMessage = False
+
     def test_result_is_pass_when_fail_not_called_on_context(self):
         def test_passes(t: TestContext):
             pass
 
-        actual = run_test(test_passes)
+        actual = run_tests([test_passes])
 
-        expected = TestResult("test_passes", is_pass=True)
+        expected = [TestResult("test_passes", is_pass=True)]
         self.assertEqual(
             expected,
             actual,
@@ -21,9 +23,9 @@ class TestRunTest(unittest.TestCase):
         def test_fails(t: TestContext):
             t.fail()
 
-        actual = run_test(test_fails)
+        actual = run_tests([test_fails])
 
-        expected = TestResult("test_fails", is_pass=False)
+        expected = [TestResult("test_fails", is_pass=False)]
         self.assertEqual(
             expected,
             actual,
@@ -36,11 +38,30 @@ class TestRunTest(unittest.TestCase):
         def test_fails(t: TestContext):
             t.fail(failure_message)
 
-        actual = run_test(test_fails)
+        actual = run_tests([test_fails])
 
-        expected = TestResult("test_fails", is_pass=True, message=failure_message)
+        expected = [TestResult("test_fails", is_pass=False, message=failure_message)]
         self.assertEqual(
             expected,
             actual,
             f"expected failing a test with message: '{failure_message}' to return {expected}, got {actual}",
+        )
+
+    def test_running_multiple_tests_returns_a_result_for_each_test(self):
+        def test_passes(t: TestContext):
+            pass
+
+        def test_fails(t: TestContext):
+            t.fail()
+
+        actual = run_tests([test_passes, test_fails])
+
+        expected = [
+            TestResult("test_passes", is_pass=True),
+            TestResult("test_fails", is_pass=False),
+        ]
+        self.assertEqual(
+            expected,
+            actual,
+            f"expected running multiple tests together to return {expected}, got {actual}",
         )
